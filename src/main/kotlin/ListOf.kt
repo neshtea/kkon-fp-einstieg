91b1e6c35b3d7511114b0ded01558980191afd59
Marco Schneider <marco.schneider@active-group.de>
2023-10-17 15:02:56 +0200

    listenfunktionen

--- src/main/kotlin/ListOf.kt
+++ src/main/kotlin/ListOf.kt
@@ -1,3 +1,5 @@
+import kotlin.math.min
+
 // singly linked list
 
 // Eine Liste aus Zahlen ist eines der folgenden:
@@ -26,3 +28,50 @@ fun <A> listLength(xs: ListOf<A>): Int =
 /**
  * Eine Liste von Dillos überfahren
  */
+fun runOverDillos(dillos: ListOf<Dillo>): ListOf<Dillo> =
+    when (dillos) {
+        is Empty -> Empty
+        is Cons ->
+            Cons(runOverDillo(dillos.first),
+                runOverDillos(dillos.rest))
+    }
+
+/**
+ * Finde alle lebenden Dillos in einer Liste.
+ */
+fun liveDillos(dillos: ListOf<Dillo>): ListOf<Dillo> =
+    when (dillos) {
+        is Empty -> Empty
+        is Cons ->
+            if (dillos.first.alive) {
+                Cons(dillos.first, liveDillos(dillos.rest))
+            } else {
+                liveDillos(dillos.rest)
+            }
+    }
+
+// Ein Resultat ist eines der folgendne
+// - nur ein Ergebnis
+// - nichts
+sealed interface Maybe<out A>
+
+// Ein Ergebnis besteht aus dem Resultat
+data class Just<A>(val value: A): Maybe<A>
+
+data object None: Maybe<Nothing>
+
+
+
+/**
+ * Das minimum einer Liste von Integern zurück geben.
+ */
+fun listMin(xs: ListOf<Int>): Maybe<Int> =
+    when (xs) {
+        is Empty -> None
+        is Cons ->
+            when (val restMin: Maybe<Int> = listMin(xs.rest)) {
+                is None -> Just(xs.first)
+                is Just ->
+                    Just(min(restMin.value, xs.first))
+            }
+    }
--- src/test/kotlin/ListOfTest.kt
+++ src/test/kotlin/ListOfTest.kt
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.Test
+import java.lang.IllegalArgumentException
 import kotlin.test.assertEquals
 
 class ListOfTest {
@@ -6,5 +7,31 @@ class ListOfTest {
     fun listLengthTest() {
         assertEquals(3, listLength(Cons(1, Cons(2, Cons(3, Empty)))))
         assertEquals(2, listLength(Cons("Marco", Cons("Markus", Empty))))
+    }
+
+    val dillo1 = Dillo(55000, true)  // 55kg, lebendig
+    val dillo2 = Dillo(58000, false)  // 58kg, tot
+    val dillo3 = Dillo(60000, true)  // 60kg, lebendig
+    val dillo4 = Dillo(63000, false)  // 63kg, tot
+
+    val highway75 = Cons(dillo1, Cons(dillo2, Cons(dillo3, Cons(dillo4, Empty))))
+
+    @Test
+    fun runOverDillosTest() {
+        assertEquals(
+            Cons(Dillo(55000, false), Cons(dillo2, Cons(Dillo(60000, false), Cons(dillo4, Empty)))),
+            runOverDillos(highway75)
+        )
+    }
+
+    @Test
+    fun liveDillosTest() {
+        assertEquals(Cons(dillo1, Cons(dillo3, Empty)), liveDillos(highway75))
+    }
+
+    @Test
+    fun listMinTest() {
+        assertEquals(None , listMin(Empty))
+        assertEquals(Just(1), listMin(Cons(1, Cons(2, Cons(3, Empty)))))
     }
 }
